185d00517a87e209835d79433851692f92ed8991
felipelfukuda <felipelfukuda@gmail.com>
2020-02-06 16:53:06 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,18 @@
+require "pry"
+
 class Shoe
+  BRANDS = []
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   def initialize(brand)
+    @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+  end
+end
+
+  def brand=(brand)
     @brand = brand
   end
 
