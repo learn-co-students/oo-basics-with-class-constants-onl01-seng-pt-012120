dfabe20276d6556740ed046482f8e5cbe90698e8
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-08 22:14:47 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDSX = []
-  BRANDS = BRANDSX.uniq
+  BRANDS = []
   
   def initialize(brand)
     @brand = brand
-    BRANDSX << brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
